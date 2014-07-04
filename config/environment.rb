db52a58358b94a9526e65fe1b22878897f5d76c1
branislav.vladisavljev <branebrvl@gmail.com>
2014-07-04 09:25:15 +0000

    Add SSL in production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 FirstApp::Application.initialize!
+
+# Speed up tests by lowering bcrypt's cost function.
+ActiveModel::SecurePassword.min_cost = true
+
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "M H"
+    email "m@e.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
